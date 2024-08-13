d86bc208a81ad2346ba62329c12107dc53e3e21b
devriazul <riazul56@gmail.com>
2024-08-14 01:04:15 +0600

    Version 1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Import this package
 
 void main() {
   runApp(MyApp());
@@ -14,64 +16,112 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/devriazul.jpg'),
-            ),
-            Text(
-              "Md Riazul Islam",
-              style: TextStyle(
-                fontSize: 36,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/devriazul.jpg'),
               ),
-            ),
-            Text(
-              "Sr. Software Developer",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-              width: 150,
-              child: Divider(color: Colors.teal[100]),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
+              Text(
+                "Md Riazul Islam",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                title: Text("01722754100"),
               ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
+              Text(
+                "Sr. Software Developer",
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                title: Text("devriazul@gmail.com"),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Thank You!",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(color: Colors.teal[100]),
               ),
-            ),
-          ],
-        )),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text("01722754100"),
+                  onTap: () => _launchDialer("01722754100"),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text("devriazul@gmail.com"),
+                  onTap: () => _launchEmail("devriazul@gmail.com"),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.web,
+                    color: Colors.teal,
+                  ),
+                  title: Text("www.devriazul.com"),
+                  onTap: () => _launchWebsite("https://www.devriazul.com"),
+                ),
+              ),
+              SizedBox(height: 20),
+              IconButton(
+                icon: FaIcon(FontAwesomeIcons.facebook,
+                    color: Colors.white, size: 40),
+                onPressed: () =>
+                    _launchWebsite("https://www.facebook.com/devriazul"),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Thank You!",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void _launchDialer(String phoneNumber) async {
+    final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunch(phoneUri.toString())) {
+      await launch(phoneUri.toString());
+    } else {
+      print('Could not launch dialer');
+    }
+  }
+
+  void _launchEmail(String email) async {
+    final Uri emailUri = Uri(scheme: 'mailto', path: email);
+    if (await canLaunch(emailUri.toString())) {
+      await launch(emailUri.toString());
+    } else {
+      print('Could not launch email client');
+    }
+  }
+
+  void _launchWebsite(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: true, forceWebView: true);
+    } else {
+      print('Could not launch website');
+    }
   }
 }
